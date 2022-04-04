d62d93dd573ed5979c92bf05628a51e7d795681e
Michael Vork <mwvork@gmail.com>
2022-04-04 12:14:28 -0400

    Added print statement to init.

--- Sources/GentleExtensions/GentleExtensions.swift
+++ Sources/GentleExtensions/GentleExtensions.swift
@@ -1,6 +1,10 @@
+import UIKit
+
 public struct GentleExtensions {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        let url = Bundle.module.url(forResource: "GentleMetadata", withExtension: "json")
+        print("GentleExtension: url: \(String(describing: url))")
     }
 }
